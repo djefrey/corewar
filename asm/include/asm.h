d3b465089e28af5a88f367e0cc41d6b265f3f663
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-05-03 11:22:25 +0200

    ASM and Corewar headers

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -8,5 +8,35 @@
 #ifndef ASM_H_
 #define ASM_H_
 
+#define MAGIC_NUMBER 0xea83f3
+
+#define COMMENT_CHAR '#'
+#define LABEL_CHAR ':'
+#define DIRECT_CHAR '%'
+#define SEPARATOR_CHAR ','
+
+#define LABEL_CHAR "abcdefghijklmnopqrstuvwxyz_0123456789"
+
+#define NAME_CMD_STRING ".name"
+#define COMMENT_CMD_STRING ".comment"
+
+#define PROG_NAME_LENGTH 128
+#define PROG_COMMENT_LENGTH 2048
+
+#define REG_NUMBER 16
+
+#define IND_SIZE 2
+#define DIR_SIZE 4
+
+#define TYPE_REG 1
+#define TYPE_DIR 2
+#define TYPE_IND 4
+
+typedef struct header_s {
+    short magic_number;
+    char prog_name[PROG_NAME_LENGTH];
+    int prog_size;
+    char prog_comment[PROG_COMMENT_LENGTH];
+} header_t;
 
 #endif /* !ASM_H_ */
--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -8,5 +8,26 @@
 #ifndef COREWAR_H_
 #define COREWAR_H_
 
+#define MAGIC_NUMBER 0xea83f3
+
+#define REG_NUMBER 16
+#define REG_SIZE 4
+
+#define IND_SIZE 2
+#define DIR_SIZE 4
+
+#define CYCLE_TO_DIE 1536
+#define CYCLE_DELTA 5
+#define NBR_LIVE 40
+
+#define PROG_NAME_LENGTH 128
+#define PROG_COMMENT_LENGTH 2048
+
+typedef struct header_s {
+    short magic_number;
+    char prog_name[PROG_NAME_LENGTH];
+    int prog_size;
+    char prog_comment[PROG_COMMENT_LENGTH];
+} header_t;
 
 #endif /* !COREWAR_H_ */
