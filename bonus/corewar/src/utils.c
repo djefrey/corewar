dfcca3eb643f2dc319a3bc5d89611ec217a80b41
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-05-22 18:21:38 +0200

    bonus: cube color darker if ID is high

--- bonus/corewar/src/utils.c
+++ bonus/corewar/src/utils.c
@@ -9,23 +9,25 @@
 
 sfVector3f generate_color(unsigned int value)
 {
+    float factor = 1.0 - (value / 6) / 10.0;
+
     if (value == 0)
         return ((sfVector3f) {0.5, 0.5, 0.5});
     value %= 6;
     switch (value) {
         case 1:
-            return ((sfVector3f) {1.0, 0.0, 0.0});
+            return ((sfVector3f) {factor, 0.0, 0.0});
         case 2:
-            return ((sfVector3f) {0.0, 1.0, 0.0});
+            return ((sfVector3f) {0.0, factor, 0.0});
         case 3:
-            return ((sfVector3f) {0.0, 0.0, 1.0});
+            return ((sfVector3f) {0.0, 0.0, factor});
         case 4:
-            return ((sfVector3f) {1.0, 0.0, 1.0});
+            return ((sfVector3f) {factor, 0.0, factor});
         case 5:
-            return ((sfVector3f) {1.0, 1.0, 0.0});
+            return ((sfVector3f) {factor, factor, 0.0});
         case 6:
-            return ((sfVector3f) {0.0, 1.0, 1.0});
+            return ((sfVector3f) {0.0, factor, factor});
         default:
-            return ((sfVector3f) {0.8, 0.8, 0.8});
+            return ((sfVector3f) {factor, factor, factor});
     }
 }
