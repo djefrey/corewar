99e52e9abc8bf3165232bda3f292b2954d749878
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-05-22 13:51:41 +0200

    bonus: fix SEGV

--- bonus/corewar/src/memory.c
+++ bonus/corewar/src/memory.c
@@ -17,6 +17,9 @@ void bonus_write_champion_id(bonus_t *bonus, int addr, int size, int id)
 {
     int value;
 
+    addr = addr % MEM_SIZE;
+    if (addr < 0)
+        addr += MEM_SIZE;
     for (int i = 0; i < size; i++) {
         value = *(bonus->memory + addr) & 0x00FFFFFF;
         value = value < 0x00FFFFFF ? (value + 1) & 0x00FFFFFF : value;
@@ -28,6 +31,9 @@ void bonus_write_champion_id(bonus_t *bonus, int addr, int size, int id)
 
 void bonus_set_actual_pc(bonus_t *bonus, int addr, char actual)
 {
+    addr = addr % MEM_SIZE;
+    if (addr < 0)
+        addr += MEM_SIZE;
     if (actual)
         *(bonus->memory + addr) |= 2147483648;
     else
